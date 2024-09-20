75d413abda1c96a96db900a09ab7981b35bfa8ce
Nour <mohamednour25@gmail.com>
2024-09-21 00:46:14 +0300

    Initial Commit

---
+++ ObservationFrameWordk/ObservationFrameWordkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationFrameWordkApp.swift
+//  ObservationFrameWordk
+//
+//  Created by administrator on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ObservationFrameWordkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
