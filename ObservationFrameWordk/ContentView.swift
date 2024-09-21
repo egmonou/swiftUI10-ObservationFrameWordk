aa95cfc20383a8da1a8d50c52592f311a4285875
Nour <mohamednour25@gmail.com>
2024-09-22 01:18:41 +0300

    125

--- ObservationFrameWordk/ContentView.swift
+++ ObservationFrameWordk/ContentView.swift
@@ -1,24 +1,61 @@
 //
 //  ContentView.swift
-//  ObservationFrameWordk
+//  ObseravationFrameWork
 //
-//  Created by administrator on 21/09/2024.
+//  Created by administrator on 19/09/2024.
 //
 
 import SwiftUI
+import Observation
 
-struct ContentView: View {
+@Observable
+class SomeViewModel {
+     var data1: String = "HellowWordls01"
+     var data2: String = "HellowWordls02"
+}
+
+struct HellowView: View {
+    
+     @State var viewModel = SomeViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(viewModel.data1)
+            Text(viewModel.data2)
+            
+            Button(action: {
+                if viewModel.data1 == "HellowWordls01" {
+                    viewModel.data1 = "change01"
+                    viewModel.data2 = "change02"
+                } else {
+                    viewModel.data1 = "HellowWordls01"
+                    viewModel.data2 = "HellowWordls02"
+                }
+
+            }, label: {
+                Text("Click Me")
+            })
         }
+        .font(.largeTitle)
         .padding()
     }
 }
 
+struct RandomNumberView: View {
+    @State private var randomNUmber = 0
+    var body: some View {
+        VStack {
+            Text("Raodm number is \(randomNUmber)")
+            Button(action: {
+                randomNUmber = (0..<1000).randomElement() ?? 0
+            }, label: {
+                Text("Randomize Number")
+            })
+            HellowView()
+        }
+    }
+}
+
 #Preview {
-    ContentView()
+    RandomNumberView()
 }
--- ObservationFrameWordk/ObservationFrameWordkApp.swift
+++ ObservationFrameWordk/ObservationFrameWordkApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ObservationFrameWordkApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HellowView()
         }
     }
 }
